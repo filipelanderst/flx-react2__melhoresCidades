898c45313df9a8f321333ce9352a09fc44fc54c5
Filipe Lander <filipelander@tutanota.de>
2023-01-19 10:16:31 -0300

    teste

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Hamburger from 'hamburger-react';
 import '../styles/menu.css';
-import Menu from './Menu';
 import { Link } from 'react-router-dom';
 
 function Header() {
@@ -29,7 +28,61 @@ function Header() {
       </div>
 
       <div className={open ? 'menu-active' : 'menu'}>
-        <Menu />
+        <nav>
+          <h2 className='text-cyan-400 text-3xl pl-2 mt-6 mb-4'>Cidades</h2>
+          <ul>
+            <li>
+              <Link to='/londres' onClick={handleMenu}>
+                1. Londres
+              </Link>
+            </li>
+            <li>
+              <Link to='/paris' onClick={handleMenu}>
+                2. Paris
+              </Link>
+            </li>
+            <li>
+              <Link to='/novaIorque' onClick={handleMenu}>
+                3. Nova Iorque
+              </Link>
+            </li>
+            <li>
+              <Link to='/toquio' onClick={handleMenu}>
+                4. Tóquio
+              </Link>
+            </li>
+            <li>
+              <Link to='/dubai' onClick={handleMenu}>
+                5. Dubai
+              </Link>
+            </li>
+            <li>
+              <Link to='/barcelona' onClick={handleMenu}>
+                6. Barcelona
+              </Link>
+            </li>
+            <li>
+              <Link to='/roma' onClick={handleMenu}>
+                7. Roma
+              </Link>
+            </li>
+            <li>
+              <Link to='/madrid' onClick={handleMenu}>
+                8. Madrid
+              </Link>
+            </li>
+            <li>
+              <Link to='/singapura' onClick={handleMenu}>
+                9. Singapura
+              </Link>
+            </li>
+            <li>
+              <Link to='/amsterda' onClick={handleMenu}>
+                10. Amsterdã
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 function Menu() {
-  const navigation = [
-    { link: '/londres', text: 'Londres' },
-    { link: '/paris', text: 'Paris' },
-    { link: '/novaIorque', text: 'Nova Iorque' },
-  ];
+  function target(e) {
+    console.log(e.target);
+  }
 
   return (
     <div>
-      <nav>
-        <button className='menu-toggle'>
-          <span className='menu hamburger'></span>
-        </button>
-        <ul className='menu-links'>
-          {navigation.map((nav) => (
-            <li key={nav.text}>
-              <a href={nav.link}>{nav.text}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
       {/* <nav>
         <h2 className='text-cyan-400 text-3xl pl-2 mt-6 mb-4'>Cidades</h2>
         <ul>
           <li>
-            <Link to='/londres'>1. Londres</Link>
+            <Link to='/londres' onClick={target}>
+              1. Londres
+            </Link>
           </li>
           <li>
             <Link to='/paris'>2. Paris</Link>
